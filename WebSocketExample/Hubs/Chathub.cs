8d3572c2696ee35809d6c262b9baa92edac972c5
Phillip <phillip_andersen@hotmail.com>
2022-10-05 09:31:51 +0200

    first commit

---
+++ WebSocketExample/Hubs/Chathub.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Diagnostics;
+
+namespace WebSocketExample.Hubs
+{
+    public class Chathub : Hub
+    {
+        public override Task OnConnectedAsync()
+        {
+            Debug.WriteLine($"Client connected: {Context.ConnectionId}"); 
+            return base.OnConnectedAsync();
+        }
+
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
